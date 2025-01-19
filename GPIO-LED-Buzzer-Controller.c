21b5d1bd91091169716617e8ae1843e1920194e1
alinnecaastro <alinnecastro133@gmail.com>
2025-01-19 16:23:30 -0300

    Adicionar o LED_VERDE

--- GPIO-LED-Buzzer-Controller.c
+++ GPIO-LED-Buzzer-Controller.c
@@ -4,38 +4,64 @@
 #include "pico/bootrom.h"
 
 
+#define GPIO_LED_VERDE 11
+
+void init_gpio(){
+
+    gpio_init(GPIO_LED_VERDE);
+    gpio_set_dir(GPIO_LED_VERDE, 1);
+    gpio_put(GPIO_LED_VERDE, 0);
+
+}
+
+
+
 int main()
 {
     stdio_init_all();
-    char funcao[30]; // Variável para armazenar a função que o usuário deseja executar
+    init_gpio(); //Inicialização dos pinos. 
+
+    char funcao[30]; // Variável para armazenar a função que o usuário deseja executar.
     printf("Sistema iniciado\n");
 
 
     while (true) {
         printf("Digite:\n VERDE - PARA LIGAR O LEDE VERDE\n AZUL - PARA LIGAR O LED AZUL\n VERMELHO - PARA LIGAR O LED VERMELHO\n BRANCO - PARA LIGAR OS TRÊS LEDS \n DESLIGAR - PARA DESLIGAR OS LEDS\n BUZZER - PARA LIGAR O BUZZER\n SAIR - PARA HABILITAR MODO GRAVAÇÃO (REBOOT)\n");
         scanf("%s", funcao);
+
         if(strcmp(funcao, "VERDE")==0)
         {
             printf("Ligando o LED VERDE\n");
+            gpio_put(GPIO_LED_VERDE, true);
+
+
         } else if(strcmp(funcao, "AZUL")==0)
         {
             printf("Ligando o LED AZUL\n");
+            gpio_put(GPIO_LED_VERDE, false);
+
         } else if(strcmp(funcao, "VERMELHO")==0)
         {
             printf("Ligando o LED VERMELHO\n");
+            gpio_put(GPIO_LED_VERDE, false);
+
         } else if(strcmp(funcao, "BRANCO")==0)
         {
             printf("Ligando os três LEDS\n");
+            gpio_put(GPIO_LED_VERDE, true);
+
         } else if(strcmp(funcao, "DESLIGAR")==0)
         {
             printf("Desligando os LEDS\n");
+            gpio_put(GPIO_LED_VERDE, false);
         } else if(strcmp(funcao, "BUZZER")==0)
         {
             printf("Ligando o BUZZER\n");
+            
+
         } else if(strcmp(funcao, "SAIR")==0)
         {
             printf("Saindo do modo de execução\n");
-            rom_reset_usb_boot(0, 0);
             
         } else
         {
