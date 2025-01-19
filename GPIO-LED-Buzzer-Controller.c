074cc642439c417497a56df1489ed010be3a191f
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-01-18 21:53:14 -0300

    Adição da instrução de uso e da variável função para armazenar a string

--- GPIO-LED-Buzzer-Controller.c
+++ GPIO-LED-Buzzer-Controller.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 
 
@@ -6,9 +7,12 @@
 int main()
 {
     stdio_init_all();
+    char funcao[30]; // Variável para armazenar a função que o usuário deseja executar
+    printf("Sistema iniciado\n");
 
+    printf("Digite:\n VERDE - PARA LIGAR O LEDE VERDE\n AZUL - PARA LIGAR O LED AZUL\n VERMELHO - PARA LIGAR O LED VERMELHO\n BRANCO - PARA LIGAR OS TRÊS LEDS \n DESLIGAR - PARA DESLIGAR OS LEDS\n BUZZER - PARA LIGAR O BUZZER\n");
     while (true) {
-        printf("Hello, world!\n");
+        scanf("%s", funcao);
         sleep_ms(1000);
     }
 }
