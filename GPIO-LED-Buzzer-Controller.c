1d7f5a38b7743c9cc5ada2185abd4bafc9052acf
sergiocunha-E <sergiocunha.m.s@gmail.com>
2025-01-19 22:55:29 -0300

    feito o código do led azul e vermelho
    adicionado o led azul e vermelho

--- GPIO-LED-Buzzer-Controller.c
+++ GPIO-LED-Buzzer-Controller.c
@@ -6,13 +6,27 @@
 
 #define GPIO_LED_VERDE 11 
 
+#define GPIO_BUZZER 21
+#define GPIO_LED_AZUL 12
+#define GPIO_LED_VERMELHO 13
 void init_gpio(){
+
+    gpio_init(GPIO_BUZZER);
+    gpio_set_dir(GPIO_BUZZER, 1);
+    gpio_put(GPIO_BUZZER, 0);
 
     
     gpio_init(GPIO_LED_VERDE);
     gpio_set_dir(GPIO_LED_VERDE, 1);
     gpio_put(GPIO_LED_VERDE, 0);
 
+    gpio_init(GPIO_LED_AZUL);
+    gpio_set_dir(GPIO_LED_AZUL, 1);
+    gpio_put(GPIO_LED_AZUL, 0);
+
+    gpio_init(GPIO_LED_VERMELHO);
+    gpio_set_dir(GPIO_LED_VERMELHO, 1);
+    gpio_put(GPIO_LED_VERMELHO, 0);
 
 }
 
@@ -34,35 +48,49 @@ int main()
         {
             printf("Ligando o LED VERDE\n");
             gpio_put(GPIO_LED_VERDE, true);
+            gpio_put(GPIO_LED_AZUL, false);
+            gpio_put(GPIO_LED_VERMELHO, false);
 
 
         } else if(strcmp(funcao, "AZUL")==0)
         {
             printf("Ligando o LED AZUL\n");
             gpio_put(GPIO_LED_VERDE, false);
+            gpio_put(GPIO_LED_AZUL, true);
+            gpio_put(GPIO_LED_VERMELHO, false);
 
         } else if(strcmp(funcao, "VERMELHO")==0)
         {
             printf("Ligando o LED VERMELHO\n");
             gpio_put(GPIO_LED_VERDE, false);
+            gpio_put(GPIO_LED_AZUL, false);
+            gpio_put(GPIO_LED_VERMELHO, true);
 
         } else if(strcmp(funcao, "BRANCO")==0)
         {
             printf("Ligando os três LEDS\n");
             gpio_put(GPIO_LED_VERDE, true);
+            gpio_put(GPIO_LED_AZUL, true);
+            gpio_put(GPIO_LED_VERMELHO, true);
 
         } else if(strcmp(funcao, "DESLIGAR")==0)
         {
             printf("Desligando os LEDS\n");
             gpio_put(GPIO_LED_VERDE, false);
+            gpio_put(GPIO_LED_AZUL, false);
+            gpio_put(GPIO_LED_VERMELHO, false);
 
         } else if(strcmp(funcao, "BUZZER")==0)
         {
             printf("Ligando o BUZZER\n");
+            gpio_put(GPIO_BUZZER, true);
+            sleep_ms(2000);
+            gpio_put(GPIO_BUZZER, false);
 
         } else if(strcmp(funcao, "SAIR")==0)
         {
             printf("Saindo do modo de execução\n");
+            rom_reset_usb_boot(0, 0);
 
         } else
         {
