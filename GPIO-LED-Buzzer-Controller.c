5380ded11a1e735bff760a58645d9e347505e804
sergiocunha-E <sergiocunha.m.s@gmail.com>
2025-01-19 23:20:55 -0300

    LED vermelho e azul adicionados
    foram adicionados o led vermelho e azul.

--- GPIO-LED-Buzzer-Controller.c
+++ GPIO-LED-Buzzer-Controller.c
@@ -4,11 +4,26 @@
 #include "pico/bootrom.h"
 
 #define GPIO_LED_VERDE 11 
+#define GPIO_LED_AZUL  12
+#define GPIO_LED_VERMELHO 13
+#define GPIO_BUZZER 21
 
 void init_gpio() {
     gpio_init(GPIO_LED_VERDE);
     gpio_set_dir(GPIO_LED_VERDE, 1);
     gpio_put(GPIO_LED_VERDE, 0);
+
+    gpio_init(GPIO_LED_AZUL);
+    gpio_set_dir(GPIO_LED_AZUL, 1);
+    gpio_put(GPIO_LED_AZUL, 0);
+
+    gpio_init(GPIO_LED_VERMELHO);
+    gpio_set_dir(GPIO_LED_VERMELHO, 1);
+    gpio_put(GPIO_LED_VERMELHO, 0);
+
+    gpio_init(GPIO_BUZZER);
+    gpio_set_dir(GPIO_BUZZER, 1);
+    gpio_put(GPIO_BUZZER, 0);
 }
 
 int main() {
@@ -47,25 +62,38 @@ int main() {
         if (strcmp(funcao, "VERDE") == 0) {
             printf("\nLigando o LED VERDE\n");
             gpio_put(GPIO_LED_VERDE, true);
+            gpio_put(GPIO_LED_AZUL, false);
+            gpio_put(GPIO_LED_VERMELHO, false);
 
         } else if (strcmp(funcao, "AZUL") == 0) {
             printf("\nLigando o LED AZUL\n");
             gpio_put(GPIO_LED_VERDE, false);
+            gpio_put(GPIO_LED_AZUL, true);
+            gpio_put(GPIO_LED_VERMELHO, false);
 
         } else if (strcmp(funcao, "VERMELHO") == 0) {
             printf("\nLigando o LED VERMELHO\n");
             gpio_put(GPIO_LED_VERDE, false);
+            gpio_put(GPIO_LED_AZUL, false);
+            gpio_put(GPIO_LED_VERMELHO, true);
 
         } else if (strcmp(funcao, "BRANCO") == 0) {
             printf("\nLigando os três LEDS\n");
             gpio_put(GPIO_LED_VERDE, true);
+            gpio_put(GPIO_LED_AZUL, true);
+            gpio_put(GPIO_LED_VERMELHO, true);
 
         } else if (strcmp(funcao, "DESLIGAR") == 0) {
             printf("\nDesligando os LEDS\n");
             gpio_put(GPIO_LED_VERDE, false);
+            gpio_put(GPIO_LED_AZUL, false);
+            gpio_put(GPIO_LED_VERMELHO, false);
 
         } else if (strcmp(funcao, "BUZZER") == 0) {
             printf("\nLigando o BUZZER\n");
+            gpio_put(GPIO_BUZZER, true);
+            sleep_ms(2000); // Buzzer ligado por 2 segundos
+            gpio_put(GPIO_BUZZER, false); 
 
         } else if (strcmp(funcao, "SAIR") == 0) {
             printf("\nSaindo do modo de execução\n");
